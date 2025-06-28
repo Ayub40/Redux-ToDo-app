17c3f705707d18ee68b18eac44f9ee7a7b0298bb
Ayub40 <ayubk4028@gmail.com>
2025-06-29 03:40:52 +0600

    first commit

---
+++ to-do-app/src/redux/features/task/taskSlice.tsx
@@ -0,0 +1,48 @@
+
+import type { RootState } from "@/redux/store";
+import type { ITask } from "@/types";
+import { createSlice } from "@reduxjs/toolkit";
+
+interface InitialState {
+    tasks: ITask[];
+    filter: "all" | "high" | "medium" | "low";
+}
+
+const initialState: InitialState = {
+    tasks: [
+        {
+            id: "asdfasdfasar",
+            title: "Initialize Frontend",
+            description: "Create Home Page and Routing",
+            dueDate: "2025-11",
+            isCompleted: false,
+            priority: "High",
+        },
+        {
+            id: "asdfasdfasar",
+            title: "Create github repo",
+            description: "Create github repo and commit",
+            dueDate: "2025-11",
+            isCompleted: false,
+            priority: "Medium",
+        },
+    ],
+    filter: "all",
+};
+
+const taskSlice = createSlice({
+    name: "task",
+    initialState,
+    reducers: {},
+});
+
+export const selectTasks = (state: RootState) => {
+    return state.todo.tasks;
+}
+
+export const selectFilter = (state: RootState) => {
+    return state.todo.filter;
+}
+
+export default taskSlice.reducer;
+
